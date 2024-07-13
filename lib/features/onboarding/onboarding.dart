7cf8d507554b82bc2147774be3f1dc11c04b0456
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-14 00:57:27 +0300

    finsh build onboarding screen

--- lib/features/onboarding/onboarding.dart
+++ lib/features/onboarding/onboarding.dart
@@ -1,3 +1,6 @@
+import 'package:doc/core/theme/app_styles.dart';
+import 'package:doc/features/onboarding/widgets/doctor_and_background_section.dart';
+import 'package:doc/features/onboarding/widgets/get_started_button.dart';
 import 'package:doc/features/onboarding/widgets/logo_name_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,11 +12,23 @@ class Onboarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 20.h),
-            const LogoNameSection(),
-          ],
+        child: Padding(
+          padding: EdgeInsets.only(top: 20.h),
+          child: Column(
+            children: [
+              const LogoNameSection(),
+              SizedBox(height: 40.h),
+              const DoctorImageAndBackgoundSection(),
+              SizedBox(height: 18.h),
+              Text(
+                "Manage and schedule all of your medical appointments easily\n with Docdoc to get a new experience.",
+                style: AppStyles.font10with400w,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 32.h),
+              const GetStartedButton()
+            ],
+          ),
         ),
       ),
     );
