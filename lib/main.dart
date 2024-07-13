f4db1d0abf7053a26d90ac2d35aa0676d0306fc6
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-13 19:41:03 +0300

    create DocApp class

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
+import 'package:doc/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const DocApp());
-}
-
-class DocApp extends StatelessWidget {
-  const DocApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
-    );
-  }
 }
