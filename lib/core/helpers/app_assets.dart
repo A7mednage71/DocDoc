11bd7a68be6b65cb4f5cc0e998f099f00957dcd5
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-14 00:54:52 +0300

    add doctor image to assets class

--- lib/core/helpers/app_assets.dart
+++ lib/core/helpers/app_assets.dart
@@ -1,6 +1,10 @@
 class Assets {
   Assets._();
   
+  /// Assets for assetsPngDoctorImage
+  /// assets/png/doctor_Image.png
+  static const String assetsPngDoctorImage = "assets/png/doctor_Image.png";
+
   /// Assets for assetsSvgsDoctorBackground
   /// assets/svgs/doctor_background.svg
   static const String assetsSvgsDoctorBackground = "assets/svgs/doctor_background.svg";
