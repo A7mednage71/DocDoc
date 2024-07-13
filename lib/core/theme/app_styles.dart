4c76349c1273f934b7e3c4821438b81a186be93a
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-13 22:33:51 +0300

    create app styles file

---
+++ lib/core/theme/app_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppStyles {
+  static TextStyle font24Black700weight = TextStyle(
+      fontSize: 32.sp,
+      fontWeight: FontWeight.w700,
+      color: const Color(0xff000000));
+}
