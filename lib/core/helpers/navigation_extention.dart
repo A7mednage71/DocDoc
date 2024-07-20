2baf4b5078133e8012bdf1b927dd62c9cf957836
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-20 15:36:50 +0300

    refactor Navigation extention

--- lib/core/helpers/navigation_extention.dart
+++ lib/core/helpers/navigation_extention.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 extension Navigation on BuildContext {
-  Future<dynamic> pushNamed(String routName, Object? arguments) {
+  Future<dynamic> pushNamed(String routName, {Object? arguments}) {
     return Navigator.of(this).pushNamed(routName, arguments: arguments);
   }
 
-  Future<dynamic> pushReplacement(String routName, Object? arguments) {
+  Future<dynamic> pushReplacement(String routName, {Object? arguments}) {
     return Navigator.of(this)
         .pushReplacementNamed(routName, arguments: arguments);
   }
 
-  Future<dynamic> pushNamedAndRemoveUntil(
-      String routName, Object? arguments, RoutePredicate predicate) {
+  Future<dynamic> pushNamedAndRemoveUntil(String routName,
+      {Object? arguments, required RoutePredicate predicate}) {
     return Navigator.of(this)
         .pushNamedAndRemoveUntil(routName, predicate, arguments: arguments);
   }
