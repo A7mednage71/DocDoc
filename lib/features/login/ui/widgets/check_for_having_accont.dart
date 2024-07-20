6e42e7dc1afa50f61e9f2fd2eecb75a8daadd4f3
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-20 21:12:27 +0300

    create CheckForHavingAccountWidget

---
+++ lib/features/login/ui/widgets/check_for_having_accont.dart
@@ -0,0 +1,28 @@
+import 'package:doc/core/theme/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class CheckForHavingAccountWidget extends StatelessWidget {
+  const CheckForHavingAccountWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Already have an account yet?",
+          style: AppStyles.font12with400wgrey,
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            "Sign Up",
+            style: AppStyles.font12with400w,
+          ),
+        )
+      ],
+    );
+  }
+}
