f7e0249654fbf51c0bdeed273e31125d48aa9409
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-22 01:05:18 +0300

    add login method in api service

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:doc/core/networking/api_constants.dart';
+import 'package:doc/features/login/data/models/login_request_body.dart';
+import 'package:doc/features/login/data/models/login_response_model.dart';
 import 'package:retrofit/http.dart';
 
 part 'api_service.g.dart';
@@ -7,6 +9,11 @@ part 'api_service.g.dart';
 @RestApi(baseUrl: ApiConstants.baseURL)
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.login)
+  Future<LoginResponseModel> login(
+    @Body() LoginRequestBody loginRequestBody,
+  );
 }
 
 
