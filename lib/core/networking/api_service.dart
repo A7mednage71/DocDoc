13a29bc911ab1577db2e6f9b5a0c470a0b022349
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-22 00:43:37 +0300

    add some comments to ApiService

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -8,3 +8,17 @@ part 'api_service.g.dart';
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 }
+
+
+
+
+
+
+
+
+
+
+//  abstract class with the @RestApi annotation.
+// This annotation tells Retrofit to generate the implementation of the API service interface.
+// factory constructor allows for the creation of an instance of _ApiService,
+// The optional baseUrl parameter allows you to override the base URL defined in ApiConstants.baseURL if needed.
