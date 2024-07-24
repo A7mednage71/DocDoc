2fbf9f3b7b19390ea1d53ee7d225cbc2292be9c1
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-24 23:37:26 +0300

    provide login cubit for login screen with getIt

--- lib/core/routing/app_route.dart
+++ lib/core/routing/app_route.dart
@@ -1,7 +1,10 @@
+import 'package:doc/core/di/dependency_injection.dart';
 import 'package:doc/core/routing/routes.dart';
+import 'package:doc/features/login/ui/manager/cubit/login_cubit.dart';
 import 'package:doc/features/login/ui/views/login_screen.dart';
 import 'package:doc/features/onboarding/onboarding.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route getroute(RouteSettings settings) {
@@ -12,7 +15,10 @@ class AppRouter {
         );
       case Routes.loginView:
         return MaterialPageRoute(
-          builder: (context) => const LoginScreen(),
+          builder: (context) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: const LoginScreen(),
+          ),
         );
 
       default:
