c0cbf9cee0fecc07a51444f2c299cf07d42639de
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-25 05:02:44 +0300

    Add password validation widget to LoginScreenForm

--- lib/features/login/ui/views/widgets/login_screen_form.dart
+++ lib/features/login/ui/views/widgets/login_screen_form.dart
@@ -1,6 +1,7 @@
 import 'package:doc/core/helpers/my_validators.dart';
 import 'package:doc/core/widgets/app_text_form_field.dart';
 import 'package:doc/features/login/ui/manager/cubit/login_cubit.dart';
+import 'package:doc/features/login/ui/views/widgets/passwords_validation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -17,6 +18,12 @@ class LoginScreenForm extends StatefulWidget {
 class _LoginScreenFormState extends State<LoginScreenForm> {
   AutovalidateMode autovalidateMode = AutovalidateMode.always;
   bool obsecured = true;
+
+  bool haslowercase = true;
+  bool hasUppercase = false;
+  bool hasSpecialChar = false;
+  bool hasNumber = false;
+  bool hasMinlength = false;
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +53,14 @@ class _LoginScreenFormState extends State<LoginScreenForm> {
             validator: (value) =>
                 MyValidators.seperatedPasswordValidator(value),
           ),
+          SizedBox(height: 25.h),
+          PasswordsValidation(
+            haslowercase: haslowercase,
+            hasUppercase: hasUppercase,
+            hasSpecialChar: hasSpecialChar,
+            hasNumber: hasNumber,
+            hasMinlength: hasMinlength,
+          )
         ],
       ),
     );
