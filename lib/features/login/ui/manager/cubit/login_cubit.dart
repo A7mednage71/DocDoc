f86f869ef37e834232178266aeb2c3379170b9d1
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-25 03:36:00 +0300

    Add TextEditingController and GlobalKey to LoginCubit for form handling

--- lib/features/login/ui/manager/cubit/login_cubit.dart
+++ lib/features/login/ui/manager/cubit/login_cubit.dart
@@ -1,11 +1,16 @@
-import 'package:bloc/bloc.dart';
 import 'package:doc/features/login/data/models/login_request_body.dart';
 import 'package:doc/features/login/data/repos/login_repo.dart';
 import 'package:doc/features/login/ui/manager/cubit/login_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   final LoginRepo _loginRepo;
   LoginCubit(this._loginRepo) : super(const LoginState.initial());
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> key = GlobalKey();
 
   Future<void> login({required LoginRequestBody loginRequestBody}) async {
     emit(const LoginState.loading());
