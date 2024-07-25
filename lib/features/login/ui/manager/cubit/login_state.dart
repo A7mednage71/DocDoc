905bbbe7390d0df8cc658c5ae597ee804c905752
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-25 17:19:57 +0300

    Refactor: Rename `_Failure` to `Failure` in `LoginState`

--- lib/features/login/ui/manager/cubit/login_state.dart
+++ lib/features/login/ui/manager/cubit/login_state.dart
@@ -7,5 +7,5 @@ class LoginState<T> with _$LoginState<T> {
   const factory LoginState.initial() = _Initial;
   const factory LoginState.loading() = Loading;
   const factory LoginState.success(T data) = Success<T>;
-  const factory LoginState.failure({required String errormessage}) = _Failure;
+  const factory LoginState.failure({required String errormessage}) = Failure;
 }
