d7f4d6b4b22235f9a5d57af31dac1f517fd26727
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-27 22:38:44 +0300

    Fix annotation casing in ApiResult class definition

--- lib/core/networking/api_result.dart
+++ lib/core/networking/api_result.dart
@@ -2,7 +2,7 @@ import 'package:doc/core/networking/api_error_handler.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'api_result.freezed.dart';
-@freezed
+@Freezed()
 abstract class ApiResult<T> with _$ApiResult<T> {
   const factory ApiResult.success(T data) = Success<T>;
   const factory ApiResult.failure(ErrorHandler error) = Failure<T>;
