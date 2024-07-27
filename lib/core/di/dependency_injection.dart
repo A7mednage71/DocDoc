559131f2889f67ee0a4415b0c16aa0df2efe6d73
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-28 01:29:09 +0300

    Add dependency injection for SignUpRepo and SignUpCubit in GetIt configuration

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -3,6 +3,8 @@ import 'package:doc/core/networking/api_service.dart';
 import 'package:doc/core/networking/dio_factory.dart';
 import 'package:doc/features/login/data/repos/login_repo.dart';
 import 'package:doc/features/login/ui/manager/cubit/login_cubit.dart';
+import 'package:doc/features/sign_up/data/repos/sign_up_repo.dart';
+import 'package:doc/features/sign_up/ui/manager/cubit/sign_up_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -10,6 +12,11 @@ final getIt = GetIt.instance;
 Future<void> setGetIt() async {
   Dio dio = await DioFactory.getDio();
   getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+  // login
   getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt<ApiService>()));
-  getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt<LoginRepo>()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt<LoginRepo>()));
+  // sign up
+  getIt
+      .registerLazySingleton<SignUpRepo>(() => SignUpRepo(getIt<ApiService>()));
+  getIt.registerFactory<SignUpCubit>(() => SignUpCubit(getIt<SignUpRepo>()));
 }
