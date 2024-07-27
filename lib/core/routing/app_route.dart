c751e13ba9fe5becd1220e784efa51579ce22dea
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-28 01:29:55 +0300

    Add SignUpCubit to AppRouter for sign-up route

--- lib/core/routing/app_route.dart
+++ lib/core/routing/app_route.dart
@@ -4,6 +4,7 @@ import 'package:doc/features/home/ui/views/home_screen.dart';
 import 'package:doc/features/login/ui/manager/cubit/login_cubit.dart';
 import 'package:doc/features/login/ui/views/login_screen.dart';
 import 'package:doc/features/onboarding/onboarding.dart';
+import 'package:doc/features/sign_up/ui/manager/cubit/sign_up_cubit.dart';
 import 'package:doc/features/sign_up/ui/views/sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -18,7 +19,7 @@ class AppRouter {
       case Routes.signUP:
         return MaterialPageRoute(
           builder: (context) => BlocProvider(
-            create: (context) => getIt<LoginCubit>(),
+            create: (context) => getIt<SignUpCubit>(),
             child: const SignUp(),
           ),
         );
