577dab63dea80fa518b6c8b83c2c034a83b134e3
A7med_Nage7 <a.nageh@bayanatz.com>
2024-07-30 03:23:51 +0300

    Add initial structure for production flavor

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,8 +2,10 @@ import 'package:doc/core/di/dependency_injection.dart';
 import 'package:doc/core/routing/app_route.dart';
 import 'package:doc/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
+  await ScreenUtil.ensureScreenSize();
   await setGetIt();
   runApp(DocApp(appRouter: AppRouter()));
 }
