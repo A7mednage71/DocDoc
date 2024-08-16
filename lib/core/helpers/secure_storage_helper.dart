731dcd1aa2b5113cf9f5ad02087ec3202e3d3eac
A7med_Nage7 <a.nageh@bayanatz.com>
2024-08-17 02:08:06 +0300

    add SecureStorageHelper file and flutter_secure_storage 9.2.2  package

---
+++ lib/core/helpers/secure_storage_helper.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageHelper {
+  static setSecuredData(String key, value) async {
+    debugPrint('FlutterSecureStorage : setSecuredData with key : $key');
+    const storage = FlutterSecureStorage();
+    await storage.write(key: key, value: value);
+  }
+
+  static getSecuredData(String key) async {
+    debugPrint('FlutterSecureStorage : getSecuredData with key : $key');
+    const storage = FlutterSecureStorage();
+    return await storage.read(key: key);
+  }
+
+  static removeSecuredData(String key) async {
+    debugPrint('FlutterSecureStorage : removeSecuredData with key : $key');
+    const storage = FlutterSecureStorage();
+    await storage.delete(key: key);
+  }
+
+  static clearAllData() async {
+    debugPrint('FlutterSecureStorage : all data has been cleared');
+    const storage = FlutterSecureStorage();
+    await storage.deleteAll();
+  }
+}
