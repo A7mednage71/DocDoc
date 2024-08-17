b93d344094a36077b373d7631bfc65efc19fe856
A7med_Nage7 <a.nageh@bayanatz.com>
2024-08-17 18:18:19 +0300

    filter doctors list when press on DoctorSpecialityItem

--- lib/features/home/ui/views/widgets/doctor_speciality_list_view.dart
+++ lib/features/home/ui/views/widgets/doctor_speciality_list_view.dart
@@ -1,37 +1,51 @@
 import 'package:doc/core/helpers/app_assets.dart';
 import 'package:doc/features/home/data/models/all_specialization_response_model.dart';
+import 'package:doc/features/home/ui/views/manager/cubit/home_cubit.dart';
 import 'package:doc/features/home/ui/views/widgets/doctor_speciality_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class DoctorSpecialityListView extends StatelessWidget {
+class DoctorSpecialityListView extends StatefulWidget {
   const DoctorSpecialityListView({super.key, this.allSpecializationData});
   final List<SpecializationItem?>? allSpecializationData;
+
+  @override
+  State<DoctorSpecialityListView> createState() =>
+      _DoctorSpecialityListViewState();
+}
+
+class _DoctorSpecialityListViewState extends State<DoctorSpecialityListView> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        children: allSpecializationData!
+        children: widget.allSpecializationData!
             .asMap()
             .entries
             .map(
-              (e) => e.key == 0
-                  ? Padding(
-                      padding: EdgeInsets.only(right: 20.w, left: 8.w),
-                      child: DoctorSpecialityItem(
-                        image: Assets.assetsPngBaby,
-                        title: e.value?.name,
-                      ),
-                    )
-                  : Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20.w),
-                      child: DoctorSpecialityItem(
-                        image: Assets.assetsPngBaby,
-                        title: e.value?.name,
-                      ),
-                    ),
+              (e) => Padding(
+                padding: e.key == 0
+                    ? EdgeInsets.only(right: 20.w, left: 8.w)
+                    : EdgeInsets.symmetric(horizontal: 20.w),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedIndex = e.key;
+                    });
+                    context.read<HomeCubit>().getDoctorsList(
+                        specializationId:
+                            widget.allSpecializationData?[selectedIndex]?.id);
+                  },
+                  child: DoctorSpecialityItem(
+                    image: Assets.assetsPngBaby,
+                    title: e.value?.name,
+                  ),
+                ),
+              ),
             )
             .toList(),
       ),
